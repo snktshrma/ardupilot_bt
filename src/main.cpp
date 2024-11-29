1fae3087e39c187bd74e678e2ce70e317c8b6ca5
snktshrma <sharma.sanket272@gmail.com>
2024-11-29 19:04:37 +0530

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <cstdio>
 #include "behaviortree_cpp/bt_factory.h"
 
+using namespace BT;
+using namespace std;
+
+
+
+class checkPrearms : public BT::SyncActionNode {
+  public:
+    checkPrearms(const string& name) : BT::SyncActionNode(name, {}) {}
+    NodeStatus tick() override {
+      cout << "Checking Prearms" << this->name() << endl;
+      return BT::NodeStatus::SUCCESS;
+    }
+};
+
 int main(int argc, char ** argv)
 {
   (void) argc;
   (void) argv;
 
-  printf("hello world ardupilot_bt package\n");
+  printf("ardupilot_bt package\n");
   return 0;
 }
