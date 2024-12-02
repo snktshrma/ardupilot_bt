d1da482eb60c252de36ec43e06d641b0bc711b3c
snktshrma <sharma.sanket272@gmail.com>
2024-12-02 10:06:59 +0530

    Updated files

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,40 @@ class checkPrearms : public BT::SyncActionNode {
     }
 };
 
+class arm : public BT::SyncActionNode {
+  // Add the sequence to confirm if arming is success, else a rate controller to
+  // arming command until armed
+  public:
+    arm(const string& name) : BT::SyncActionNode(name, {}) {}
+    NodeStatus tick() override {
+      cout << "Arming the vehicle" << this->name() << endl;
+      return BT::NodeStatus::SUCCESS;
+    }
+};
+
+class takeoff : public BT::SyncActionNode {
+  // TODO: Add altitude input for user
+  // Add the sequence and fallbacks to confirm if takeoff is success
+
+  public:
+    takeoff(const string& name) : BT::SyncActionNode(name, {}) {}
+    NodeStatus tick() override {
+      cout << "Taking off the vehicle" << this->name() << endl;
+      return BT::NodeStatus::SUCCESS;
+    }
+};
+
+class vehicleMode : public BT::SyncActionNode {
+  // Changes vehicle modes (like auto, guided, RTL, LAND, etc.)
+
+  public:
+    vehicleMode(const string& name) : BT::SyncActionNode(name, {}) {}
+    NodeStatus tick() override {
+      cout << "Vehcile's mode changes" << this->name() << endl;
+      return BT::NodeStatus::SUCCESS;
+    }
+};
+
 int main(int argc, char ** argv)
 {
   (void) argc;
