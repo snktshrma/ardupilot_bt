3acec74fa7a33e5c68934c647068828d30680723
snktshrma <sharma.sanket272@gmail.com>
2024-12-10 21:27:36 +0530

    Completed initial BT setup

---
+++ include/ardupilot_bt/nodes.hpp
@@ -0,0 +1,134 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <behaviortree_ros2/bt_action_node.hpp>
+#include <iostream>
+#include <rclcpp/executors.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include "behaviortree_cpp/basic_types.h"
+#include "behaviortree_cpp/tree_node.h"
+#include "std_msgs/msg/string.hpp"
+#include <atomic>
+#include <chrono>
+#include <cmath>
+#include <functional>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
+// #include "behaviortree_cpp/loggers/bt_file_logger.h"
+#include "ardupilot_bt/nodes.hpp"
+#include <math.h>
+
+using namespace BT;
+using namespace std;
+
+
+NodeStatus isModeChanged() {
+    cout << "Checking if Mode is already set [isModeChanged: FAILURE]" << endl;
+    return NodeStatus::FAILURE;
+}
+
+NodeStatus checkPrearm() {
+    cout << "Prearm Passed [checkPrearm: SUCCESS]" << endl;
+    return NodeStatus::SUCCESS;
+}
+
+class setMode : public BT::SyncActionNode {
+    public:
+        setMode(const string& name, const NodeConfig& config)
+            : BT::SyncActionNode(name, config), mode_num(4) {}
+
+        static PortsList providedPorts() {
+            return {OutputPort<int>("modeNum") };
+        }
+
+        NodeStatus tick() override {
+            // std::string s = std::to_string(mode_num);
+            setOutput("modeNum", mode_num);
+            cout << "Mode Set through output port: " << mode_num << endl;
+            return NodeStatus::SUCCESS;
+        }
+    private:
+        int mode_num;
+};
+
+
+class setAlt : public BT::SyncActionNode {
+    public:
+        setAlt(const string& name, const NodeConfig& config)
+            : BT::SyncActionNode(name, config), alt_(10) {}
+
+        static PortsList providedPorts() {
+            return {OutputPort<float>("alt") };
+        }
+
+        NodeStatus tick() override {
+            // std::string s = std::to_string(alt);
+            setOutput("alt", alt_);
+            cout << "Altitude Set through output port: " << alt_ << endl;
+            return NodeStatus::SUCCESS;
+        }
+    private:
+        float alt_;
+};
+
+class changeMode : public BT::SyncActionNode {
+  public:
+    changeMode(const string& name, const NodeConfig& config) : BT::SyncActionNode(name, config) {}
+    static PortsList providedPorts() {
+        return { InputPort<int>("modeNum") };
+    }
+
+    NodeStatus tick() override {
+        Expected<int> mode_num = getInput<int>("modeNum");
+
+        if (!mode_num) {
+            throw BT::RuntimeError("Mode not set: ", mode_num.error() );
+        }
+        cout << "Mode Changed: " << mode_num.value() << endl;
+        return BT::NodeStatus::SUCCESS;
+    }
+};
+
+class arm : public BT::SyncActionNode {
+  // Add the sequence to confirm if arming is success, else a rate controller to
+  // arming command until armed
+  public:
+    arm(const string& name) : BT::SyncActionNode(name, {}) {}
+    NodeStatus tick() override {
+      cout << "Arming the vehicle: " << this->name() << endl;
+      return BT::NodeStatus::SUCCESS;
+    }
+};
+
+class takeoff : public BT::SyncActionNode {
+  // TODO: Add altitude input for user
+  // Add the sequence and fallbacks to confirm if takeoff is success
+
+  public:
+    takeoff(const string& name, const NodeConfig& config) : BT::SyncActionNode(name, config) {}
+
+    static PortsList providedPorts() {
+        return { InputPort<float>("alt") };
+    }
+
+    NodeStatus tick() override {
+        Expected<float> alt_ = getInput<float>("alt");
+
+        if (!alt_) {
+            throw BT::RuntimeError("Alt to defined: ", alt_.error() );
+        }
+        cout << "Taking Off! Altitude: " << alt_.value() << endl;
+        return BT::NodeStatus::SUCCESS;
+    }
+};
+
+#endif /* NODE_H */
--- src/main_ros.cpp
+++ src/main_ros.cpp
@@ -1,97 +1,7 @@
-#include <behaviortree_ros2/bt_action_node.hpp>
-#include <iostream>
-#include <rclcpp/executors.hpp>
-#include <rclcpp/rclcpp.hpp>
-
-#include "behaviortree_cpp/basic_types.h"
-#include "behaviortree_cpp/tree_node.h"
-// #include "std_msgs/msg/string.hpp"
-#include <atomic>
-#include <chrono>
-#include <cmath>
-#include <functional>
-#include <memory>
-#include <string>
-#include <thread>
-
-// #include "geometry_msgs/msg/twist.hpp"
-// #include "nav_msgs/msg/odometry.hpp"
-// #include "sensor_msgs/msg/laser_scan.hpp"
-// #include <geometry_msgs/msg/pose_stamped.hpp>
-// #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-
-// #include "behaviortree_cpp/loggers/bt_file_logger.h"
-#include <math.h>
+#include "ardupilot_bt/nodes.hpp"
 
 using namespace BT;
 using namespace std;
-
-
-
-class changeMode : public BT::SyncActionNode {
-  public:
-    changeMode(const string& name) : BT::SyncActionNode(name, {}) {}
-    NodeStatus tick() override {
-      cout << "Changing Mode" << this->name() << endl;
-      return BT::NodeStatus::SUCCESS;
-    }
-};
-
-BT::NodeStatus checkPrearm() {
-  std::cout << "[ Checking Status ]" << std::endl;
-  return BT::NodeStatus::SUCCESS;
-}
-
-class arm : public BT::SyncActionNode {
-  // Add the sequence to confirm if arming is success, else a rate controller to
-  // arming command until armed
-  public:
-    arm(const string& name) : BT::SyncActionNode(name, {}) {}
-    NodeStatus tick() override {
-      cout << "Arming the vehicle" << this->name() << endl;
-      return BT::NodeStatus::SUCCESS;
-    }
-};
-
-class takeoff : public BT::SyncActionNode {
-  // TODO: Add altitude input for user
-  // Add the sequence and fallbacks to confirm if takeoff is success
-
-  public:
-    takeoff(const string& name) : BT::SyncActionNode(name, {}) {}
-    NodeStatus tick() override {
-      cout << "Taking off the vehicle" << this->name() << endl;
-      return BT::NodeStatus::SUCCESS;
-    }
-};
-
-class vehicleMode : public BT::SyncActionNode {
-  // Changes vehicle modes (like auto, guided, RTL, LAND, etc.)
-
-  public:
-    vehicleMode(const string& name) : BT::SyncActionNode(name, {}) {}
-    NodeStatus tick() override {
-      cout << "Vehicle's mode changes" << this->name() << endl;
-      return BT::NodeStatus::SUCCESS;
-    }
-};
-
-static const char* xml = R"(<?xml version="1.0" encoding="UTF-8"?>
-<root BTCPP_format="4">
-  <BehaviorTree ID="main">
-    <Sequence name="root_sequence">
-      <changeMode name="mode_change" modeNum="1"/>
-      <RetryUntilSuccessful num_attempts="10">
-        <checkPrearm name="prearm_check"/>
-      </RetryUntilSuccessful>
-      <arm name="arm"/>
-      <Takeoff name="takeoff"/>
-    </Sequence>
-  </BehaviorTree>
-
-</root>
-
-)";
 
 int main()
 {
@@ -100,10 +10,14 @@ int main()
   factory.registerNodeType<changeMode>("changeMode");
   factory.registerNodeType<arm>("arm");
   factory.registerNodeType<takeoff>("takeoff");
+  factory.registerNodeType<setMode>("setMode");
+  factory.registerNodeType<setAlt>("setAlt");
 
+  factory.registerSimpleCondition("isModeChanged", std::bind(isModeChanged));
   factory.registerSimpleCondition("checkPrearm", std::bind(checkPrearm));
 
-  auto tree = factory.createTreeFromText(xml);
+  // auto tree = factory.createTreeFromText(xml);
+  auto tree = factory.createTreeFromFile("/home/snkt/bt_ws/src/ardupilot_bt/src/tree.xml");
 
   std::chrono::milliseconds ms = (std::chrono::milliseconds) 100;
   // BT::Tree::TickOption ops = BT::Tree::TickOption::EXACTLY_ONCE;
